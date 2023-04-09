a664f88a890c0eafce90c0502c00952f21f9f379
Shrikant <sawantshrikant07@gmail.com>
2023-04-09 19:50:55 +0530

    [Shrikant] add:ValidateFirstName

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using UserRegistrationProblem;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome To User Validation Program!");
+        Validation validation = new Validation();
+        validation.ValidateFirstName("Shrikant");
+        validation.ValidateFirstName("om");
+        validation.ValidateFirstName("sai");
+        validation.ValidateFirstName("Ram");
+    }
+}
---
+++ UserRegistrationProblem/Validation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class Validation
+    {
+        public static string FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public void ValidateFirstName(string name)
+        {
+            if (Regex.IsMatch(name, FIRST_NAME))
+                Console.WriteLine("Valid first name");
+            else
+                Console.WriteLine("Invalid first name");
+        }
+    }
+}
